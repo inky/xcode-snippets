5a7132508e6d053f1d8dcf63f48f20eaefb88473
Liam Cooke <liamcooke@gmail.com>
2013-06-06 22:56:19 +0100

    Fix an XML-encoded ampersand

--- singleton.m
+++ singleton.m
@@ -7,7 +7,7 @@
 {
     static id sharedInstance = nil;
     static dispatch_once_t onceToken;
-    dispatch_once(&amp;onceToken, ^{
+    dispatch_once(&onceToken, ^{
         sharedInstance = [[self alloc] <#init#>];
     });
     return sharedInstance;
